d8702e500acd401a5e9b74c6f3cee8da21f1c909
thao <nguyen.thi.ngoc.thao@sun-asterisk.com>
2025-02-15 15:46:09 +0700

    Component - Popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import PopUp from "./components/PopUp";
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <PopUp/>
       <Footer />
     </>
   );
